7b2e6dc1911021ee73020e40a709c491dd4c6af1
Rehan <rehandin2007@gmail.com>
2026-05-24 16:52:52 +0500

    Added CORS headers and connection fix

--- backend/api/auth/register.php
+++ backend/api/auth/register.php
@@ -1,4 +1,18 @@
 <?php
+
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// Agar request OPTIONS (preflight) hai toh yahin se response ok bhej dein
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 require_once '../../config/Database.php';
 require_once '../../helpers/Cors.php';
 require_once '../../helpers/JwtHandler.php';
